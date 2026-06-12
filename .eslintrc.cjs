04d011acaa6007d700b3be0866d326c3327d5d57
Michał Mleczko <do@mleczki.pl>
2026-06-12 10:47:58 +0200

    chore: Update ESLint config to ignore build and dependency directories

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  ignorePatterns: ['dist/**', '.astro/**', 'node_modules/**'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
