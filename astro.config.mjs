aafee29b2798a8ec6d5a042e177254e34bd000b4
Michał Mleczko <do@mleczki.pl>
2026-06-18 01:33:08 +0200

    feat: implement chat interface in vanilla js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
-import react from '@astrojs/react';
-
-// Sprawdzamy, czy aplikacja jest uruchomiona w trybie deweloperskim
-const isDev = process.env.NODE_ENV === 'development';
 
 export default defineConfig({
   output: 'static',
   site: 'https://eduacademy.pl',
-  // Ładujemy integrację Keystatic TYLKO podczas lokalnej pracy.
-  // Podczas 'npm run build' zostanie ona pominięta, więc Astro
-  // zbuduje czysty, statyczny kod bez żądania adaptera Node.js.
   integrations: [
     tailwind(),
     sitemap(),
-    react(),
     markdoc(),
-    ...(isDev ? [keystatic()] : [])
   ],
   build: {
     inlineStylesheets: 'auto',
