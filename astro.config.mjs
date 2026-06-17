e2eaf81fd730f5fe66698837d85fbd1b907f117a
Michał Mleczko <do@mleczki.pl>
2026-06-17 10:30:14 +0200

    feat: introduce keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import keystatic from '@keystatic/astro';
+import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  output: 'static',
   site: 'https://eduacademy.pl',
-  integrations: [tailwind(), sitemap(), react()],
+  integrations: [tailwind(), sitemap(), react(), markdoc(), keystatic()],
   build: {
     inlineStylesheets: 'auto',
   },
