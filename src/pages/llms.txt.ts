542bf01be8e638f62a5db7d3cbf82e9f773c4fd6
Michał Mleczko <do@mleczki.pl>
2026-06-18 02:29:58 +0200

    chore: fix llms.txt content type

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -104,7 +104,7 @@ export async function GET() {
 
   return new Response(content, {
     headers: {
-      'Content-Type': 'text/plain; charset=utf-8',
+      'Content-Type': 'text/markdown; charset=utf-8',
       'Cache-Control': 'public, max-age=3600',
     },
   });
